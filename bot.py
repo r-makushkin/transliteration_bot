9a31943a65ac2e0b3b71d6f7374a0bb82fe98e89
r-makushkin <petropotap@gmail.com>
2023-10-16 19:58:57 +0300

    update func def trans

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-def trans(m):
+def trans(message):
     d = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g',
      'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh', 
      'з': 'z', 'и': 'i', 'й': 'i', 'к': 'k', 
@@ -21,21 +21,24 @@ def trans(m):
      'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 
      'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': 'ie', 
      'ы': 'y', 'э': 'e', 'ю': 'iu', 'я': 'ia', ' ':' '}
+    if not message.isalpha():
+        return 'Вы ввели неверный текст. Он не должен содержать символы и цифры.'
     
-    m = list(m.lower())
+    message = list(message.lower())
     
-    while 'ь' in m:
-        m.remove('ь')
+    while 'ь' in message:
+        message.remove('ь')
     
-    a = list(d[el] for el in m if el in d)
-    return ''.join(a).title()
+    res = ''.join([d[el] for el in message if el in d]).title()
+    return res
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     user_name = message.from_user.first_name
     user_id = message.from_user.id
     logging.info(f'{user_name=} {user_id=} sent message:{message.text}')
-    text = f'Привет, {user_name}! Я помогу произвести транслитерацию в соответствии с приказом МИД РФ. Пришли необходимые ФИО'
+    text = f'Привет, {user_name}! Я помогу произвести транслитерацию текста в соответствии с приказом МИД РФ от 12.02.2020 № 2113. \
+        Пришлите необходимый текст без цифр и символов'
     await message.reply(text)
 
 @dp.message_handler()
